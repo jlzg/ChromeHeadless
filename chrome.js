80ffa4c46851b4e7a532de51b1acd4c26e7f5720
lwpro2 <lwpro2@gmail.com>
2017-10-30 22:08:00 +0800

    fun with chrome headless

---
+++ chrome.js
@@ -0,0 +1,21 @@
+const cdp = require('chrome-remote-interface');
+const fs = require('fs');
+
+
+cdp({
+        host: 'localhost',
+        port: 9222
+    },
+    async client => {
+    const {Page} = client;
+
+    await Page.enable();
+
+    let {data} = await Page.captureScreenshot({
+        format: 'png',
+    });
+
+    fs.writeFile('screenshot.png',data, 'base64', e=> console.error(e));
+
+    await client.close();
+});
